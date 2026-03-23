adc9ab6f3eb10af815f37a00123bd36306592c97
yuri-gabriel2010-escola <yuri.zagati@escola.pr.gov.br>
2026-03-23 18:08:47 -0300

    Add files via upload

---
+++ SQLite (1).sql
@@ -0,0 +1,6 @@
+CREATE TABLE alunos (
+ matricula integer PRIMARY key AUTOINCREMENT,
+ nome text,
+ idade integer,
+ turma text 
+)
---
+++ SQLite.5.sql
@@ -0,0 +1,2 @@
+INSERT into alunos (nome, idade, turma)
+VALUES('carlos', 17, '2°C')
